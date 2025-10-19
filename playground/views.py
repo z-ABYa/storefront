6f0b531fe2817179f47e5198713bca34bc1386c8
z-ABYa <189abhish@gmail.com>
2025-10-19 19:41:51 +0530

    Update views.py

--- playground/views.py
+++ playground/views.py
@@ -1,19 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.core.exceptions import ObjectDoesNotExist
-from store.models import Customer, OrderItem, Product, Order
+from django.contrib.contenttypes.models import ContentType
+from store.models import Product
+from tags.models import TaggedItem
 
 
 def say_hello(request):
-    queryset = Customer.objects.filter(first_name__istartswith='h')
-    # queryset = OrderItem.objects.values('product_id__title').order_by('product_id__title')
+    content_type =  ContentType.objects.get_for_model(Product)
 
-    # order_queryset = OrderItem.objects.values('product_id').distinct()
-    # queryset = Product.objects.filter(id__in=order_queryset).order_by('title')
+    queryset = TaggedItem.objects \
+        .select_related('tag') \
+        .filter(
+            content_type=content_type,
+            object_id=1
+        )
 
-    q = OrderItem.objects.values('order__customer__first_name', 'product__title')
-    return render(request, 'hello.html', {'name': 'Abhishek', 'order_items': list(q)})
-
-    q = Customer.objects.only('first_name', 'membership')
-    return render(request, 'hello.html', {'name': 'Abhishek', 'customers': list(q)})
-
+    return render(request, 'hello.html', {"name": 'abhishek', 'tags': list(queryset)})
