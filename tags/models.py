dd6a25743d4f2c5ae2387e09ae7d707b19121732
z-ABYa <189abhish@gmail.com>
2025-10-20 16:05:20 +0530

    Update models.py

--- tags/models.py
+++ tags/models.py
@@ -4,10 +4,24 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
 
+# Custom Manager
+class TaggedItemManager(models.Manager):
+    def get_tags_for(self, obj_type, obj_id):
+        content_type =  ContentType.objects.get_for_model(obj_type)
+
+        queryset = TaggedItem.objects \
+            .select_related('tag') \
+            .filter(
+                content_type=content_type,
+                object_id=obj_id
+            )
+
+
 class Tag(models.Model):
     label = models.CharField(max_length=255)
 
 class TaggedItem(models.Model):
+    objects = TaggedItemManager() 
     # What tag is applied to which obj
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
     # product = models.ForeignKey(Product) <- not a good implementation as it is dependent upon playground app.
