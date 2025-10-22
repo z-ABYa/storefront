84a35f27deb500a2c61d1782747915f13c7672a3
z-ABYa <189abhish@gmail.com>
2025-10-22 20:36:48 +0530

    update store/admin.py

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price', 'inventory_status']
+    list_editable = ['unit_price']
+    list_per_page = 10
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'LOW'
+        return 'OK'
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    list_per_page = 10
+
+admin.site.register(models.Collection)
