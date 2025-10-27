1754d98268e6466da95befddf64f38367b9cdca5
z-ABYa <189abhish@gmail.com>
2025-10-27 23:18:11 +0530

    Update models.py

--- store/models.py
+++ store/models.py
@@ -17,8 +17,8 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
-    description = models.TextField()
     slug = models.SlugField()
+    description = models.TextField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
