16d318a1cee11b1665c7e8452b9f7c4a0e6c3170
z-ABYa <189abhish@gmail.com>
2025-11-17 23:31:16 +0530

    Update views.py

--- store/views.py
+++ store/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view()
+def product_list(request):
+    return Response("ok")
+
+@api_view()
+def product_details(request, id):
+    return Response(id)
