aa15b55de8b5747b6ca79e60a09abd2cb06ce645
z-ABYa <189abhish@gmail.com>
2025-12-09 11:25:11 +0530

    Update urls.py

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns =[
     path('products/', views.product_list),
-    path('products/<int:id>/', views.product_details),
-    path('collections/<int:id>/', views.collection_details, name='collection-detail'),
+    path('products/<int:id>/', views.product_detail),
+    path('collections/', views.collection_list),
+    path('collections/<int:id>/', views.collection_detail, name='collection-detail'),
 ]
