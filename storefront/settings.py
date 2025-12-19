cdb4c6258cdde06249160bf3e456dc17882b2c0c
z-ABYa <189abhish@gmail.com>
2025-12-19 18:29:42 +0530

    extending user model

--- storefront/settings.py
+++ storefront/settings.py
@@ -42,9 +42,9 @@ INSTALLED_APPS = [
     'playground',
     'debug_toolbar',
     'store',
-    'store_custom',
     'tags',
-    'likes'
+    'likes',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -140,3 +140,5 @@ INTERNAL_IPS = [
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False
 }
+
+AUTH_USER_MODEL = 'core.User'
