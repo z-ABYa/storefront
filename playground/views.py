886551540c3399a1a75cd297d8a0d3e9347c88f9
z-ABYa <189abhish@gmail.com>
2025-12-30 18:41:46 +0530

    sending email using templated_mail

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,15 @@
+from django.core.mail import EmailMessage, BadHeaderError
 from django.shortcuts import render
-from store.models import Product, Order, OrderItem
-from tags.models import TaggedItem
+from templated_mail.mail import BaseEmailMessage
 
 
 def say_hello(request):
-
+    try:
+        message = BaseEmailMessage(
+                    template_name='emails/hello.html', 
+                    context={'name': 'abhishek'}
+                )
+        message.send(to=['bob@domain.com'])
+    except BadHeaderError:
+        pass
     return render(request, 'hello.html', {"name": 'abhishek'})
--- storefront/settings.py
+++ storefront/settings.py
@@ -170,9 +170,13 @@ DJOSER = {
     }
 }
 
-EMAIL_BACKEND = 'django.core.mail.beckends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'localhost'
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 2525
 DEFAULT_FROM_EMAIL = 'from@abhi.com'
+
+ADMINS = [
+    ('Abhishek', 'admin@abhi.com')
+]
