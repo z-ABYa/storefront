8f248d77d05cc40eed6bfbb861f149f745e55025
z-ABYa <189abhish@gmail.com>
2026-01-01 14:59:13 +0530

    celery beat for periodic tasks

--- storefront/settings.py
+++ storefront/settings.py
@@ -182,3 +182,10 @@ ADMINS = [
 ]
 
 CELERY_BROKER_URL = 'redis://localhost:6379/1'
+CELERY_BEAT_SCHEDULE = {
+    'notify_customers': {
+        'task': 'playground.tasks.notify_customers',
+        'schedule': 5,
+        'args': ['Messages']
+    }
+}
