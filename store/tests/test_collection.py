e35e08018f02f284a75111a298187e09721597eb
z-ABYa <189abhish@gmail.com>
2026-01-03 15:04:09 +0530

    test for user ≠ admin

--- store/tests/test_collection.py
+++ store/tests/test_collection.py
@@ -5,11 +5,14 @@ import pytest
 @pytest.mark.django_db
 class TestCreateCollection:
     def test_if_user_is_anonymous_returns_401(self):
-        # Arrange
-
-        # Act
         client = APIClient()
         response = client.post('/store/collections/', {'title': 'a'})
-
-        # Assert
+        
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403(self):
+        client = APIClient()
+        client.force_authenticate(user={})
+        response = client.post('/store/collections/', {'title': 'a'})
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
