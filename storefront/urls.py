9cb7b0a0e516ca85cd926cccd0f7cb3d392c9b14
z-ABYa <189abhish@gmail.com>
2026-01-07 18:52:21 +0530

    configuring home page

--- storefront/urls.py
+++ storefront/urls.py
@@ -24,6 +24,7 @@ admin.site.site_header = 'Storefront Admin'
 admin.site.index_title = 'Admin'
 
 urlpatterns = [
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
     path('playground/', include('playground.urls')),
     path('auth/', include('djoser.urls')),
@@ -33,4 +34,5 @@ urlpatterns = [
 ] 
 
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
