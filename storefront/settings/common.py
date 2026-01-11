9d1a7d38dc5d21caf2cf6c5ca9b2f3022a24d056
z-ABYa <189abhish@gmail.com>
2026-01-11 15:33:51 +0530

    production with render

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -109,8 +109,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -1,3 +1,4 @@
+import dj_database_url
 import os
 from .common import *
 
@@ -6,4 +7,12 @@ DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    "default": dj_database_url.config(
+        default="sqlite:///db.sqlite3",
+        conn_max_age=600,
+        ssl_require=True,
+    )
+}
